9fa901f6b5c3368fd859b447d0922e0d1fce57e3
unknown <sardor2010228@gmail.com>
2025-03-19 17:34:43 +0500

    thirst

--- src/home.tsx
+++ src/home.tsx
@@ -1,4 +1,6 @@
 import './home.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Shop from './shop';
 
 function App() {
 
@@ -12,12 +14,22 @@ function App() {
                 <img src="/src/img/logo2.svg" alt="" />
               </div>
               <div className="li-box">
-                <ul>
-                  <li><a href="">Home</a></li>
-                  <li><a href="">Shop</a></li>
-                  <li><a href="">About</a></li>
-                  <li><a href="">Contact</a></li>
-                </ul>
+                <Router>
+                  <div>
+                    <nav>
+                      <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to='/shop'>Shop</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                      </ul>
+                    </nav>
+                    <Routes>
+                      <Route path="/shop" element={<Shop />} />
+                    </Routes>
+                  </div>
+                </Router>
+
               </div>
               <div className="icon-box">
                 <a href=""><img src="/src/img/human.svg" alt="" /></a>
@@ -139,11 +151,74 @@ function App() {
             </div>
             <div className="sh-box">
               <button className='show'>Show More</button>
-
             </div>
           </div>
         </div>
       </section>
+      <section className='section3'>
+        <div className="container">
+          <div className='div'>
+            <div className="bea-box">
+              <h1>50+ Beautiful rooms
+                inspiration</h1><br />
+              <p>Our designer already made a lot of beautiful <br />prototipe of rooms that inspire you</p><br />
+              <button>Explore More</button>
+            </div>
+            <div className="inner-box">
+              <img src="/src/img/inner.svg" alt="" style={{ paddingTop: '48px', paddingLeft: '100px' }} />
+              <img src="/src/img/content.svg" style={{ marginLeft: '-380px', marginBottom: '22px' }} alt="" />
+              <img src="/src/img/CTA.svg" style={{ marginBottom: '22px' }} alt="" />
+              <img src="/src/img/room.svg" alt="" className='room' />
+              <button className='arrow'><img src="/src/img/arrow.svg" alt="" /></button>
+              <img src="/src/img/room2.png" alt="" className='room2' />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='section4'>
+        <div className="container">
+          <h3>Share your setup with</h3>
+          <h1>#FuniroFurniture</h1>
+        </div>
+        <div className="dir">
+          <img src="/src/img/images.svg" alt="" />
+        </div>
+      </section>
+      <footer className='footer'>
+        <div className="container">
+          <div className="foot-box">
+            <div className="fur-box">
+              <h2>Funiro.</h2><br /><br />
+              <p>400 University Drive Suite 200 Coral <br /> Gables,<br />
+                FL 33134 USA</p>
+            </div>
+            <div className="link-box">
+              <p>Links</p><br /><br /><br />
+              <a href="">Home</a><br /><br /><br />
+              <a href="">Shop</a><br /><br /><br />
+              <a href="">About</a><br /><br /><br />
+              <a href="">Contact</a><br /><br /><br />
+            </div>
+            <div className="help-box">
+              <p>Help</p><br /><br /><br />
+              <a href="">Payment Options</a><br /><br /><br />
+              <a href="">Returns</a><br /><br /><br />
+              <a href="">Privacy Policies</a><br /><br /><br />
+            </div>
+            <div className="help-box">
+              <p>Newsletter</p>
+              <div className="input-box">
+                <form action="">
+                  <input type="text" placeholder='Enter Your Email Address' />
+                  <button>SUBSCRIBE</button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="line"></div>
+          <p style={{ color: '#000000' }}>2023 furino. All rights reverved</p>
+        </div>
+      </footer>
     </>
   )
 }
---
+++ src/shop.tsx
@@ -0,0 +1,11 @@
+import './shop.css'
+
+function Shop() {
+    return (
+        <>
+            <h1>efgjer</h1>
+        </>
+    )
+}
+
+export default Shop
